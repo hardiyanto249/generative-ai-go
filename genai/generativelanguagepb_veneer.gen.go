28e73bac4cc870df15e655d4c1a4b4074214398a
Yan <hardiyanto.id@gmail.com>
2025-10-05 20:09:53 +0700

    fix: not use VideoDuration

--- genai/generativelanguagepb_veneer.gen.go
+++ genai/generativelanguagepb_veneer.gen.go
@@ -1658,7 +1658,7 @@ func (v *VideoMetadata) toProto() *pb.VideoFileMetadata {
 		return nil
 	}
 	return &pb.VideoFileMetadata{
-		VideoDuration: durationpb.New(v.Duration),
+		//VideoDuration: durationpb.New(v.Duration),
 	}
 }
 
@@ -1668,7 +1668,7 @@ func (VideoMetadata) fromProto(p *pb.VideoFileMetadata) *VideoMetadata {
         return nil
     }
     return &VideoMetadata{
-		Duration: pvDurationFromProto(p.VideoDuration),
+		Duration: 0, //pvDurationFromProto(p.VideoDuration),
     }
 }
 
